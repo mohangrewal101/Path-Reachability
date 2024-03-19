b2fd7bdc4aff7f7284442aa7518526bd48c0541e
alex-clements <ahclemen@ualberta.ca>
2024-03-18 17:11:20 -0700

    Add test programs to examine analyzer output

---
+++ example_programs/test_program_2.ts
@@ -0,0 +1,47 @@
+function example2(a: number, b: number, c: boolean, d: boolean) {
+  if (a > b) {
+    // do something
+  } else {
+    // do something else
+  }
+
+  if (b == a) {
+    // do something
+    if (c) {
+      // do something
+    } else {
+      // do something else
+    }
+  } else {
+    if (a > 0) {
+      // do something
+    } else {
+      // do something else
+    }
+
+    const e: boolean = false;
+
+    if (e) {
+      // do something
+    } else {
+      // do something else
+    }
+  }
+
+  if (d) {
+    // do something
+    if (c) {
+      // do something
+    }
+  } else {
+    // do something else
+  }
+
+  const f: boolean = true;
+
+  if (f) {
+    // do something
+  } else {
+    // do something else
+  }
+}
